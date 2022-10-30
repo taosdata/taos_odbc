c7a7018ffe54dbe7e72cc02b511a99fab0a73c7d
freemine <freemine@yeah.net>
2022-10-30 10:31:41 +0800

    refactor cmake-related

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -22,50 +22,108 @@
 # SOFTWARE.
 ###############################################################################
 
-macro(check_odbc_requirement)
+macro(check_requirements)
+  if(NOT WIN32)
+    string(ASCII 27 Esc)
+    set(ColorReset  "${Esc}[m")
+    set(ColorBold   "${Esc}[1m")
+    set(Red         "${Esc}[31m")
+    set(Green       "${Esc}[32m")
+    set(Yellow      "${Esc}[33m")
+    set(Blue        "${Esc}[34m")
+    set(Magenta     "${Esc}[35m")
+    set(Cyan        "${Esc}[36m")
+    set(White       "${Esc}[37m")
+    set(BoldRed     "${Esc}[1;31m")
+    set(BoldGreen   "${Esc}[1;32m")
+    set(BoldYellow  "${Esc}[1;33m")
+    set(BoldBlue    "${Esc}[1;34m")
+    set(BoldMagenta "${Esc}[1;35m")
+    set(BoldCyan    "${Esc}[1;36m")
+    set(BoldWhite   "${Esc}[1;37m")
+  endif()
+
+  set(TAOS_ODBC_LOCAL_REPO $ENV{HOME}/.taos_odbc)
+  include(CheckSymbolExists)
+
+  ## prepare `cjson`
+  include(ExternalProject)
+  set(CJSON_INSTALL_PATH ${TAOS_ODBC_LOCAL_REPO}/.install)
+  ExternalProject_Add(ex_cjson
+      GIT_REPOSITORY https://github.com/taosdata-contrib/cJSON.git
+      GIT_TAG v1.7.15
+      GIT_SHALLOW TRUE
+      PREFIX "${TAOS_ODBC_LOCAL_REPO}/cjson"
+      CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CJSON_INSTALL_PATH}"
+      )
+
+  ## check `taos`
+  find_library(TAOS NAMES taos)
+  if(${TAOS} STREQUAL TAOS-NOTFOUND)
+    message(FATAL_ERROR "${Red}`libtaos.so` is required but not found, you may refer to https://github.com/taosdata/TDengine${ColorReset}")
+  endif()
+
+  set(CMAKE_REQUIRED_LIBRARIES taos)
+  check_symbol_exists(taos_init "taos.h" HAVE_TAOS)
+  if(NOT HAVE_TAOS)
+    message(FATAL_ERROR "${Red}`taos.h` is required but not found, you may refer to https://github.com/taosdata/TDengine${ColorReset}")
+  endif()
+
+  ## check `iconv`
+  # find_package(Iconv)
+  # if(NOT Iconv_FOUND)
+  #   message(FATAL_ERROR "${Red}you need to install `iconv` first${ColorReset}")
+  # endif()
+
+  ## check `flex`
   find_package(FLEX)
   if(NOT FLEX_FOUND)
-    message(FATAL_ERROR "you need to install `flex` first")
+    message(FATAL_ERROR "${Red}you need to install `flex` first${ColorReset}")
   endif()
   if(CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 5.0.0)
-    message(FATAL_ERROR "gcc 4.8.0 will complain too much about flex-generated code, we just bypass building ODBC driver in such case")
+    message(FATAL_ERROR "${Red}gcc 4.8.0 will complain too much about flex-generated code, we just bypass building ODBC driver in such case${ColorReset}")
   endif()
 
+  ## check `bison`
   find_package(BISON)
   if(NOT BISON_FOUND)
-    message(FATAL_ERROR "you need to install `bison` first")
+    message(FATAL_ERROR "${Red}you need to install `bison` first${ColorReset}")
   endif()
   if(CMAKE_C_COMPILER_ID STREQUAL "GNU" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 5.0.0)
-    message(FATAL_ERROR "gcc 4.8.0 will complain too much about bison-generated code, we just bypass building ODBC driver in such case")
+    message(FATAL_ERROR "${Red}gcc 4.8.0 will complain too much about bison-generated code, we just bypass building ODBC driver in such case${ColorReset}")
   endif()
 
+  ## check `odbcinst`
   find_program(TAOS_ODBC_ODBCINST_INSTALLED NAMES odbcinst)
   if(NOT TAOS_ODBC_ODBCINST_INSTALLED)
     if(TAOS_ODBC_DARWIN)
-      message(FATAL_ERROR "unixodbc is not installed yet, you may install it under macOS by typing: brew install unixodbc")
+      message(FATAL_ERROR "${Red}unixodbc is not installed yet, you may install it under macOS by typing: brew install unixodbc${ColorReset}")
     else()
-      message(FATAL_ERROR "odbcinst is not installed yet, you may install it under Ubuntu by typing: sudo apt install odbcinst")
+      message(FATAL_ERROR "${Red}odbcinst is not installed yet, you may install it under Ubuntu by typing: sudo apt install odbcinst${ColorReset}")
     endif()
   endif()
 
+  ## check `isql`
   find_program(TAOS_ODBC_ISQL_INSTALLED NAMES isql)
   if(NOT TAOS_ODBC_ISQL_INSTALLED)
     if(TAOS_ODBC_DARWIN)
-      message(FATAL_ERROR "unixodbc is not installed yet, you may install it under macOS by typing: brew install unixodbc")
+      message(FATAL_ERROR "${Red}unixodbc is not installed yet, you may install it under macOS by typing: brew install unixodbc${ColorReset}")
     else()
-      message(FATAL_ERROR "unixodbc is not installed yet, you may install it under Ubuntu by typing: sudo apt install unixodbc")
+      message(FATAL_ERROR "${Red}unixodbc is not installed yet, you may install it under Ubuntu by typing: sudo apt install unixodbc${ColorReset}")
     endif()
   endif()
 
+  ## check `pkg-config`
   find_program(TAOS_ODBC_PKG_CONFIG_INSTALLED NAMES pkg-config)
   if(NOT TAOS_ODBC_PKG_CONFIG_INSTALLED)
     if(TAOS_ODBC_DARWIN)
-      message(FATAL_ERROR "pkg-config is not installed yet, you may install it under macOS by typing: brew install pkg-config")
+      message(FATAL_ERROR "${Red}pkg-config is not installed yet, you may install it under macOS by typing: brew install pkg-config${ColorReset}")
     else()
-      message(FATAL_ERROR "pkg-config is not installed yet, you may install it under Ubuntu by typing: sudo apt install pkg-config")
+      message(FATAL_ERROR "${Red}pkg-config is not installed yet, you may install it under Ubuntu by typing: sudo apt install pkg-config${ColorReset}")
     endif()
   endif()
 
+  ## get `odbc/odbcinst` info via `pkg-config`
   execute_process(COMMAND pkg-config --variable=includedir odbc ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE ODBC_INCLUDE_DIRECTORY)
   execute_process(COMMAND pkg-config --variable=libdir odbc ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE ODBC_LIBRARY_DIRECTORY)
   execute_process(COMMAND pkg-config --libs-only-L odbc ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE ODBC_LINK_OPTIONS)
@@ -74,20 +132,58 @@ macro(check_odbc_requirement)
   execute_process(COMMAND pkg-config --variable=libdir odbcinst ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE ODBCINST_LIBRARY_DIRECTORY)
   execute_process(COMMAND pkg-config --libs-only-L odbcinst ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE ODBCINST_LINK_OPTIONS)
 
-  include(CheckSymbolExists)
-
   set(CMAKE_REQUIRED_LIBRARIES odbc odbcinst)
   set(CMAKE_REQUIRED_INCLUDES ${ODBC_INCLUDE_DIRECTORY} ${ODBCINST_INCLUDE_DIRECTORY})
   set(CMAKE_REQUIRED_LINK_OPTIONS ${ODBC_LINK_OPTIONS} ${ODBCINST_LINK_OPTIONS})
 
+  ## check `sql.h`
   check_symbol_exists(SQLExecute "sql.h" HAVE_ODBC_DEV)
   if(NOT HAVE_ODBC_DEV)
-    message(FATAL_ERROR "odbc requirement not satisfied, please install unixodbc-dev. Check detail in ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log")
+    message(FATAL_ERROR "${Red}odbc requirement not satisfied, please install unixodbc-dev. Check detail in ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log${ColorReset}")
   endif()
 
+  ## check `odbcinst.h`
   check_symbol_exists(SQLInstallODBC "odbcinst.h" HAVE_ODBCINST_DEV)
   if(NOT HAVE_ODBCINST_DEV)
-    message(FATAL_ERROR "odbc requirement not satisfied, check detail in ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log")
+    message(FATAL_ERROR "${Red}odbc requirement not satisfied, check detail in ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log${ColorReset}")
+  endif()
+
+  ## check `valgrind`
+  find_program(VALGRIND NAMES valgrind)
+  if(NOT VALGRIND)
+    message(STATUS "${Yellow}`valgrind` tool not found, thus valgrind-related-test-cases would be eliminated, you may refer to https://valgrind.org/${ColorReset}")
+  else()
+    set(HAVE_VALGRIND ON)
+  endif()
+
+  ## check `node`
+  find_program(NODEJS NAMES node)
+  if(NOT NODEJS)
+    message(STATUS "${Yellow}`node` not found, thus nodejs-related-test-cases would be eliminated, you may refer to https://nodejs.org/${ColorReset}")
+  else()
+    set(HAVE_NODEJS ON)
+    execute_process(COMMAND node --version ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE NODEJS_VERSION)
+    message(STATUS "${Green}`node` found -- ${NODEJS_VERSION}, please be noted, nodejs v12 and above are expected compatible${ColorReset}")
+  endif()
+
+  ## check `rustc`
+  find_program(RUSTC NAMES rustc)
+  if(NOT RUSTC)
+    message(STATUS "${Yellow}`rustc` not found, thus rustc-related-test-cases would be eliminated, you may refer to https://rust-lang.org/${ColorReset}")
+  else()
+    set(HAVE_RUSTC ON)
+    execute_process(COMMAND rustc --version ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE RUSTC_VERSION)
+    message(STATUS "${Green}`rustc` found -- ${RUSTC_VERSION}, please be noted, rustc v1.63 and above are expected compatible${ColorReset}")
+  endif()
+
+  ## check `cargo`
+  find_program(CARGO NAMES cargo)
+  if(NOT CARGO)
+    message(STATUS "${Yellow}`cargo` not found, thus cargo-related-test-cases would be eliminated, you may refer to https://rust-lang.org/${ColorReset}")
+  else()
+    set(HAVE_CARGO ON)
+    execute_process(COMMAND cargo --version ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE CARGO_VERSION)
+    message(STATUS "${Green}`cargo` found -- ${CARGO_VERSION}, please be noted, cargo v1.63 and above are expected compatible${ColorReset}")
   endif()
 
 endmacro()

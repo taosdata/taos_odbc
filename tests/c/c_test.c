ba7d171db28c0a2131e569859c69dcf59d6ffc7a
freemine <freemine@yeah.net>
2023-05-12 12:54:30 +0800

    test case

--- tests/c/c_test.c
+++ tests/c/c_test.c
@@ -29,7 +29,8 @@
 #include <stdint.h>
 
 
-#define DUMP(fmt, ...)          printf(fmt "\n", ##__VA_ARGS__)
+#define DUMP(fmt, ...)             fprintf(stderr, fmt "\n", ##__VA_ARGS__)
+#define DCASE(fmt, ...)            fprintf(stderr, "L%d:%s():" fmt "\n", line, func, ##__VA_ARGS__)
 
 typedef struct handles_s                 handles_t;
 struct handles_s {
@@ -127,7 +128,7 @@ static int _dump_result_sets(handles_t *handles)
     DUMP("Rows affected:%" PRId64 "", (int64_t)RowCount);
 
     SQLSMALLINT ColumnCount = 0;
-    sr = CALL_SQLNumResultCols(handles->hstmt, &ColumnCount);  
+    sr = CALL_SQLNumResultCols(handles->hstmt, &ColumnCount);
     if (FAILED(sr)) return -1;
     DUMP("ColumnCount:%d", ColumnCount);
 
@@ -328,11 +329,128 @@ static int test_case0(void)
   return 0;
 }
 
+static int _check_with_values_ap(int line, const char *func, handles_t *handles, const char *sql, size_t nr_rows, size_t nr_cols, va_list ap)
+{
+  SQLRETURN sr = SQL_SUCCESS;
+
+  sr = CALL_SQLExecDirect(handles->hstmt, (SQLCHAR*)sql, SQL_NTS);
+  if (FAILED(sr)) return -1;
+
+  SQLSMALLINT ColumnCount = 0;
+  sr = CALL_SQLNumResultCols(handles->hstmt, &ColumnCount);
+  if (FAILED(sr)) return -1;
+  if ((size_t)ColumnCount != nr_cols) {
+    DCASE("expected %zd columns, but got ==%d==", nr_cols, ColumnCount);
+    return -1;
+  }
+
+  for (size_t i_row = 0; i_row < nr_rows; ++i_row) {
+    sr = CALL_SQLFetch(handles->hstmt);
+    if (sr==SQL_NO_DATA) {
+      DCASE("expected %zd rows, but got ==%zd==", nr_rows, i_row);
+      return -1;
+    }
+    if (FAILED(sr)) return -1;
+    for (size_t i_col=0; i_col<(size_t)ColumnCount; ++i_col) {
+      char colName[1024]; colName[0] = '\0';
+      SQLSMALLINT NameLength      = 0;
+      SQLSMALLINT DataType        = 0;
+      SQLULEN     ColumnSize      = 0;
+      SQLSMALLINT DecimalDigits   = 0;
+      SQLSMALLINT Nullable        = 0;
+      sr = CALL_SQLDescribeCol(handles->hstmt, (SQLUSMALLINT)i_col+1, (SQLCHAR*)colName, sizeof(colName), &NameLength, &DataType, &ColumnSize, &DecimalDigits, &Nullable);
+      if (FAILED(sr)) return -1;
+
+      char buf[4096]; buf[0] = '\0';
+      SQLLEN len = 0;
+      sr = CALL_SQLGetData(handles->hstmt, (SQLUSMALLINT)i_col+1, SQL_C_CHAR, buf, sizeof(buf), &len);
+      if (FAILED(sr)) return -1;
+      const char *v = va_arg(ap, const char*);
+      if (len == SQL_NULL_DATA) {
+        if (v) {
+          DCASE("[%zd,%zd]:expected [%s], but got ==null==", i_row+1, i_col+1, v);
+          return -1;
+        }
+        continue;
+      }
+      if (len == SQL_NTS) len = (SQLLEN)strlen(buf);
+      if (strlen(v) != (size_t)len || strncmp(buf, v, (size_t)len)) {
+        DCASE("[%zd,%zd]:expected [%s], but got ==%.*s==", i_row+1, i_col+1, v, (int)len, buf);
+        return -1;
+      }
+    }
+  }
+
+  sr = CALL_SQLFetch(handles->hstmt);
+  if (sr == SQL_NO_DATA) return 0;
+  if (sr != SQL_SUCCESS && sr != SQL_SUCCESS_WITH_INFO) {
+    return -1;
+  }
+
+  DCASE("expected %zd rows, but got ==more rows==", nr_rows);
+  return -1;
+}
+
+static int _check_with_values(int line, const char *func, handles_t *handles, const char *sql, size_t nr_rows, size_t nr_cols, ...)
+{
+  va_list ap;
+  va_start(ap, nr_cols);
+  int r = _check_with_values_ap(line, func, handles, sql, nr_rows, nr_cols, ap);
+  va_end(ap);
+
+  return r ? -1 : 0;
+}
+
+#define CHECK_WITH_VALUES(...)       _check_with_values(__LINE__, __func__, ##__VA_ARGS__)
+
+static int test_charsets(handles_t *handles)
+{
+  int r = 0;
+
+  const char *conn_str = NULL;
+  const char *sqls = NULL;
+  const char *sql = NULL;
+
+  handles_release(handles);
+
+  conn_str = "DSN=TAOS_ODBC_DSN";
+  r = handles_init(handles, conn_str);
+
+  sqls =
+    "drop database if exists foo;"
+    "create database if not exists foo;"
+    "create table foo.t (ts timestamp, name varchar(20), mark nchar(20));"
+    "insert into foo.t (ts, name, mark) values (now(), 'name', 'mark');"
+    "insert into foo.t (ts, name, mark) values (now()+1s, '测试', '检验');";
+  r = _execute_batches_of_statements(handles, sqls);
+  if (r) return -1;
+
+  sql = "select name from foo.t where name='name'",
+  r = CHECK_WITH_VALUES(handles, sql, 1, 1, "name");
+  if (r) return -1;
+
+  sql = "select mark from foo.t where mark='mark'",
+  r = CHECK_WITH_VALUES(handles, sql, 1, 1, "mark");
+  if (r) return -1;
+
+  sql = "select name from foo.t where name='测试'",
+  r = CHECK_WITH_VALUES(handles, sql, 1, 1, "测试");
+  if (r) return -1;
+
+  sql = "select mark from foo.t where mark='检验'",
+  r = CHECK_WITH_VALUES(handles, sql, 1, 1, "检验");
+  if (r) return -1;
+
+  return 0;
+}
+
 static int running_with_handles(handles_t *handles)
 {
   (void)handles;
   int r = 0;
   r = test_case0();
+  if (r) return -1;
+  r = test_charsets(handles);
   if (r) return -1;
 
   return 0;
@@ -358,3 +476,4 @@ int main(int argc, char *argv[])
   fprintf(stderr, "==%s==\n", r ? "failure" : "success");
   return !!r;
 }
+

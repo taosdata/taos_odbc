2f0180fc167018516dd8042a8639f55cdadd2243
freemine <freemine@yeah.net>
2023-05-12 16:22:31 +0800

    py3 test case

--- tests/python/py3_test.py
+++ tests/python/py3_test.py
@@ -37,73 +37,156 @@ if False:
   x = cursor.execute("select name,mark from bar.x").fetchall()
   print(x, file=sys.stderr)
 
-# Specifying the ODBC driver, server name, database, etc. directly
-cnxn = pyodbc.connect('DRIVER={TAOS_ODBC_DRIVER};SERVER=localhost;DATABASE=information_schema;UID=root;PWD=taosdata')
 
-# Using a DSN, but providing a password as well
-cnxn = pyodbc.connect('DSN=TAOS_ODBC_DSN;PWD=taosdata')
+def test_case0():
+  # Specifying the ODBC driver, server name, database, etc. directly
+  cnxn = pyodbc.connect('DRIVER={TAOS_ODBC_DRIVER};SERVER=localhost;DATABASE=information_schema;UID=root;PWD=taosdata')
 
-# Create a cursor from the connection
-cursor = cnxn.cursor()
+  # Using a DSN, but providing a password as well
+  cnxn = pyodbc.connect('DSN=TAOS_ODBC_DSN;PWD=taosdata')
 
-cursor.execute("drop database if exists bar")
-cursor.execute("create database if not exists bar")
-cursor.execute("use bar")
+  # Create a cursor from the connection
+  cursor = cnxn.cursor()
 
-cursor.execute("select * from information_schema.ins_tables")
-while True:
+  cursor.execute("drop database if exists bar")
+  cursor.execute("create database if not exists bar")
+  cursor.execute("use bar")
+
+  cursor.execute("select * from information_schema.ins_tables")
+  while True:
+    row = cursor.fetchone()
+    if not row:
+      break
+    print(row, file=sys.stderr)
+
+  cursor.execute("drop table if exists x")
+  cursor.execute("create table x (ts timestamp, name varchar(20), mark nchar(20))")
+  cursor.execute("insert into x(ts, name, mark) values (now(), '测试', '试验')")
+  # cnxn.commit()
+  cursor.execute("insert into x(ts, name, mark) values (?, ?, ?)", 1682565350033, 'xes', 'no')
+  # cnxn.commit()
+  x = cursor.execute("select name,mark from x").fetchall()
+  print(x, file=sys.stderr)
+  y = [('xes', 'no'), ('测试', '试验')]
+  assert str(x) == str(y), "{0} != {1}".format(x, y)
+
+  cursor.execute("drop stable if exists st")
+  cursor.execute("create stable st (ts timestamp, age int) tags (name varchar(20))")
+  cursor.execute("insert into 'suzhou' using st tags ('jiangsu') values (1665226861289, 100)")
+  x = cursor.execute("select name,age from st").fetchall()
+  print(x, file=sys.stderr)
+  y = [('jiangsu', 100)]
+  assert str(x) == str(y), "{0} != {1}".format(x, y)
+
+  cursor.execute("drop stable if exists st")
+  cursor.execute("create stable st (ts timestamp, age int) tags (name varchar(20))")
+  cursor.execute("insert into ? using st tags (?) values (?, ?)", 'suzhou', 'jiangsu', 1665226861289, 100)
+  x = cursor.execute("select age,name from st").fetchall()
+  print(x, file=sys.stderr)
+  y = [(100, 'jiangsu')]
+  assert str(x) == str(y), "{0} != {1}".format(x, y)
+
+  cursor.execute("drop stable if exists st")
+  cursor.execute("create stable st (ts timestamp, age int) tags (name varchar(20))")
+  params = [ ('suzhou', 'jiangsu', 1665226861289, 101), ('shanghai', 'Shanghai', 1665226861299, 202) ]
+  cursor.fast_executemany = False
+  cursor.executemany("insert into ? using st tags (?) values (?, ?)", params)
+
+  x = cursor.execute("select name,age from st").fetchall()
+  print(x, file=sys.stderr)
+  y = [('jiangsu', 101), ('Shanghai', 202)]
+  assert str(x) == str(y), "{0} != {1}".format(x, y)
+
+  cursor.execute("drop table if exists x")
+  cursor.execute("create table x (ts timestamp, name varchar(20), mark nchar(20))")
+  cursor.execute("insert into x(ts, name, mark) values (now(), '测试', '试验')")
+  # cnxn.commit()
+  cursor.execute("insert into x(ts, name, mark) values (?, ?, ?)", 1682565350033, '试验', '测试')
+  # cnxn.commit()
+  x = cursor.execute("select name,mark from x").fetchall()
+  print(x, file=sys.stderr)
+  y = [('试验', '测试'), ('测试', '试验')]
+  assert str(x) == str(y), "{0} != {1}".format(x, y)
+
+def check_with_values(cnxn, sql, nr_rows, nr_cols, *vals):
+  cursor = cnxn.execute(sql)
+  assert len(cursor.description) == nr_cols, f"expected {nr_cols} columns, but got =={len(cursor.description)}=="
   row = cursor.fetchone()
-  if not row:
-    break
-  print(row, file=sys.stderr)
+  i_row = 0
+  idx = 0
+  while row:
+    assert i_row < nr_rows, "expected {0} rows, but got ==more rows==".format(nr_rows)
+      
+    for i_col in range(0, nr_cols):
+      assert row[i_col] == vals[idx], f"[{i_row+1,i_col+1}]:expected [{vals[idx]}], but got =={row[i_col]}=="
+      idx += 1
+    i_row += 1
+    row = cursor.fetchone()
 
-cursor.execute("drop table if exists x")
-cursor.execute("create table x (ts timestamp, name varchar(20), mark nchar(20))")
-cursor.execute("insert into x(ts, name, mark) values (now(), '测试', '试验')")
-# cnxn.commit()
-cursor.execute("insert into x(ts, name, mark) values (?, ?, ?)", 1682565350033, 'xes', 'no')
-# cnxn.commit()
-x = cursor.execute("select name,mark from x").fetchall()
-print(x, file=sys.stderr)
-y = [('xes', 'no'), ('测试', '试验')]
-assert str(x) == str(y), "{0} != {1}".format(x, y)
+  assert i_row == nr_rows, "expected {0} rows, but got =={1}==".format(nr_rows, i_row)
 
-cursor.execute("drop stable if exists st")
-cursor.execute("create stable st (ts timestamp, age int) tags (name varchar(20))")
-cursor.execute("insert into 'suzhou' using st tags ('jiangsu') values (1665226861289, 100)")
-x = cursor.execute("select name,age from st").fetchall()
-print(x, file=sys.stderr)
-y = [('jiangsu', 100)]
-assert str(x) == str(y), "{0} != {1}".format(x, y)
+  pass
 
-cursor.execute("drop stable if exists st")
-cursor.execute("create stable st (ts timestamp, age int) tags (name varchar(20))")
-cursor.execute("insert into ? using st tags (?) values (?, ?)", 'suzhou', 'jiangsu', 1665226861289, 100)
-x = cursor.execute("select age,name from st").fetchall()
-print(x, file=sys.stderr)
-y = [(100, 'jiangsu')]
-assert str(x) == str(y), "{0} != {1}".format(x, y)
+def test_charsets():
+  cnxn = pyodbc.connect('DSN=TAOS_ODBC_DSN;PWD=taosdata')
+  cnxn.execute("drop database if exists foo")
+  cnxn.execute("create database if not exists foo")
+  cnxn.execute("create table foo.t (ts timestamp, name varchar(20), mark nchar(20))")
+  cnxn.execute("insert into foo.t (ts, name, mark) values (now(), 'name', 'mark')")
+  cnxn.execute("insert into foo.t (ts, name, mark) values (now()+1s, '测试', '检验')")
+  check_with_values(cnxn, "select name from foo.t where name='name'", 1, 1, "name")
+  check_with_values(cnxn, "select mark from foo.t where mark='mark'", 1, 1, "mark")
+  check_with_values(cnxn, "select name from foo.t where name='测试'", 1, 1, "测试")
+  check_with_values(cnxn, "select mark from foo.t where mark='检验'", 1, 1, "检验")
 
-cursor.execute("drop stable if exists st")
-cursor.execute("create stable st (ts timestamp, age int) tags (name varchar(20))")
-params = [ ('suzhou', 'jiangsu', 1665226861289, 101), ('shanghai', 'Shanghai', 1665226861299, 202) ]
-cursor.fast_executemany = False
-cursor.executemany("insert into ? using st tags (?) values (?, ?)", params)
 
-x = cursor.execute("select name,age from st").fetchall()
-print(x, file=sys.stderr)
-y = [('jiangsu', 101), ('Shanghai', 202)]
-assert str(x) == str(y), "{0} != {1}".format(x, y)
+def find_case(test_cases, name):
+  for t in test_cases:
+    if t.__name__ == name:
+      return t
+  return None
 
-cursor.execute("drop table if exists x")
-cursor.execute("create table x (ts timestamp, name varchar(20), mark nchar(20))")
-cursor.execute("insert into x(ts, name, mark) values (now(), '测试', '试验')")
-# cnxn.commit()
-cursor.execute("insert into x(ts, name, mark) values (?, ?, ?)", 1682565350033, '试验', '测试')
-# cnxn.commit()
-x = cursor.execute("select name,mark from x").fetchall()
-print(x, file=sys.stderr)
-y = [('试验', '测试'), ('测试', '试验')]
-assert str(x) == str(y), "{0} != {1}".format(x, y)
+def run_case(t):
+  r = t()
+  if r is None:
+    return 0
+  if r == 0:
+    return 0
+  print(f"{t.__name__} => {r}:failed")
+  exit(1)
+
+def usage(arg0):
+  print(f"{arg0} -h\n"
+        f"  show this help page\n"
+        f"{arg0} [name]...\n"
+        f"  running test case `name`\n"
+        f"{arg0} -l\n"
+        f"  list all test cases")
+
+if __name__ == "__main__":
+  test_cases = [test_case0, test_charsets]
+
+  if len(sys.argv) == 1:
+    for t in test_cases:
+      run_case(t)
+    exit(0)
+
+  for i, arg in enumerate(sys.argv):
+    if i == 0:
+      continue
+    if arg == "-h":
+      usage(sys.argv[0])
+      exit(0)
+    if arg == "-l":
+      print("supported test cases:")
+      for t in test_cases:
+        print(f"  {t.__name__}")
+      exit(0)
+    t = find_case(test_cases, arg)
+    if t is None:
+      print(f"test case `{arg}`:not found")
+      exit(1)
+    run_case(t)
 
 print("success")
+

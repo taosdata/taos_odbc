33472feadbabf8e3591c3fd38d80940d9c0a7919
freemine <freemine@yeah.net>
2023-05-13 08:01:40 +0800

    test case and python3 version check

--- tests/go/go_test_odbc.go
+++ tests/go/go_test_odbc.go
@@ -5,6 +5,10 @@ import (
     "database/sql"
     "log"
     "fmt"
+    "os"
+    "reflect"
+    "runtime"
+    "strconv"
     )
 
 func test_sql_server() {
@@ -71,7 +75,7 @@ func test_chars() {
   defer db.Close()
 }
 
-func main() {
+func test() {
   if false {
     test_sql_server()
     log.Fatal("==success==")
@@ -124,3 +128,84 @@ func main() {
 
   fmt.Println("==success==")
 }
+
+func test_case0() int {
+  return 0
+}
+
+func test_case1() int {
+  return 0
+}
+
+func usage(arg0 string) {
+  fmt.Fprintf(os.Stderr,
+      "%v -h\n"                           +
+      "  show this help page\n"           +
+      "%v [name]...\n"                    +
+      "  running test case `name`\n"      +
+      "%v -l\n"                           +
+      "  list all test cases\n",
+      arg0, arg0, arg0)
+}
+
+func find_case(cases []func() int, name string) func() int {
+  for _,c := range cases {
+    if runtime.FuncForPC(reflect.ValueOf(c).Pointer()).Name() == "main." + name {
+      return c
+    }
+  }
+  return nil
+}
+
+func list_cases(cases []func() int) {
+  fmt.Println("supported test cases:")
+  for _,c := range cases {
+    fmt.Println("  " + runtime.FuncForPC(reflect.ValueOf(c).Pointer()).Name()[5:])
+  }
+}
+
+func run_case(c func() int) {
+  r := c()
+  if r != 0 {
+    log.Fatal(runtime.FuncForPC(reflect.ValueOf(c).Pointer()).Name() + " => " + strconv.Itoa(r) + ":failed")
+  }
+}
+
+func run_cases(cases []func () int) {
+  for _,c := range cases {
+    run_case(c)
+  }
+}
+
+func main() {
+  cases := []func () int { test_case0, test_case1 }
+  nr_args := len(os.Args)
+  nr_cases := 0
+  if nr_args > 1 {
+    for i:=1; i<nr_args; i+=1 {
+      arg := os.Args[i]
+      if arg == "-h" {
+        usage(os.Args[0])
+        os.Exit(0)
+      }
+      if arg == "-l" {
+        list_cases(cases)
+        os.Exit(0)
+      }
+      c := find_case(cases, arg)
+      if c == nil {
+        log.Fatal("test case `" + arg + "`:not found")
+      }
+      run_case(c)
+      nr_cases += 1
+    }
+  }
+
+  if nr_cases == 0 {
+    run_cases(cases)
+  }
+
+  fmt.Println("==success==")
+  os.Exit(1)
+}
+

ed898922bc3c9f2307e29c8e2b26600180d8c095
freemine <freemine@yeah.net>
2023-05-27 20:27:00 +0800

    refact haskell test case

--- tests/hs/app/Main.hs
+++ tests/hs/app/Main.hs
@@ -29,45 +29,51 @@ import Data.Time
 import Text.Printf
 
 myAssert :: Monad m => Bool -> String -> m ()
-myAssert p s
-  | p == True = return ()
-  | otherwise = error s
+myAssert True _ = return ()
+myAssert _    s = error s
 
 getNextTick :: Maybe String -> IO String
-getNextTick Nothing = fmap (formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S.%3q") $ getZonedTime
+getNextTick Nothing = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S.%3q" <$> getZonedTime
 getNextTick (Just prev) = do
-  t <- fmap (formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S.%3q") $ getZonedTime
+  t <- formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S.%3q" <$> getZonedTime
   if t > prev then
     return t
   else
     getNextTick (Just prev)
+
+runAndCheck :: IConnection conn => conn -> String -> Integer -> IO ()
+runAndCheck conn sql affected_rows = do
+  v <- run conn sql []
+  myAssert (v==affected_rows) $ printf "\n  sql:%s\n  affected rows expected:%d,\n  but got ==============:%d:" sql affected_rows v
+
+quickQueryAndCheck :: IConnection conn => conn -> String -> [[SqlValue]] -> IO ()
+quickQueryAndCheck conn sql rs_expected = do
+  rs <- quickQuery conn sql []
+  myAssert (rs == rs_expected) $ printf "\n  sql:%s:\n  rs expected:%s,\n  but got ===:%s:" sql (show rs_expected) (show rs)
+
+execAndCheck :: Statement -> [SqlValue] -> Integer -> IO ()
+execAndCheck stmt params affected_rows = do
+  v <- execute stmt params
+  myAssert (v==affected_rows) $ printf "\n  affected rows expected:%d,\n  but got ==============:%d:" affected_rows v
+
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   conn <- connectODBC "DSN=TAOS_ODBC_DSN;Database=bar"
 
-  v1 <- run conn "drop table if exists haskell" []
-  printf "affected rows:%d\n" v1
-  myAssert (v1==0) $ ("affected rows is expected to be 0, but got ==" ++ (show v1) ++ "==")
-
-  v2 <- run conn "create table if not exists haskell (ts timestamp, name varchar(20))" []
-  printf "affected rows:%d\n" v2
-  myAssert (v2==0) $ ("affected rows is expected to be 0, but got ==" ++ (show v2) ++ "==")
+  runAndCheck conn "drop table if exists haskell" 0
+  runAndCheck conn  "create table if not exists haskell (ts timestamp, name varchar(20))" 0
 
   stmt <- prepare conn "insert into haskell (ts, name) values (?, ?)"
-
   t3 <- getNextTick Nothing
-  v3 <- execute stmt [toSql t3, toSql "hello"]
-  printf "affected rows:%d\n" v3
-  myAssert (v3==1) $ ("affected rows is expected to be 1, but got ==" ++ (show v3) ++ "==")
-
+  execAndCheck stmt [toSql t3, toSql "hello"] 1
   t4 <- getNextTick (Just t3)
-  v4 <- execute stmt [toSql t4, toSql "中国"]
-  printf "affected rows:%d\n" v4
-  myAssert (v4==1) $ ("affected rows is expected to be 1, but got ==" ++ (show v4) ++ "==")
+  execAndCheck stmt [toSql t4, toSql "中国"] 1
+  quickQueryAndCheck conn "select * from haskell" [[toSql t3, toSql "hello"],[toSql t4, toSql "中国"]]
 
-  rs <- quickQuery conn "select * from haskell" []
-  print $ show rs
-  myAssert (rs == [[toSql t3, toSql "hello"],[toSql t4, toSql "中国"]]) "data queried is not as expected"
+  runAndCheck conn "drop table if exists haskell" 0
+  runAndCheck conn "create table if not exists haskell (ts timestamp, name varchar(20))" 0
+  runAndCheck conn (printf "insert into haskell (ts, name) values ('%s', '中国')" t4) 1
+  quickQueryAndCheck conn "select * from haskell" [[toSql t4, toSql "中国"]]
 
